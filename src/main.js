ef83daf55bbbaede952756bdac83495129c013f4
wellmota <impeto.wellington@gmail.com>
2024-09-12 15:55:14 -0300

    chore: Update package.json with project dependencies and scripts, added webpack

---
+++ src/main.js
@@ -0,0 +1,18 @@
+"use strict"
+
+// IMPORT DAYJS
+
+import "./libs/dayjs.js"
+
+// CSS
+
+import "./style/global.css"
+import "./style/home.css"
+import "./style/header.css"
+import "./style/aside.css"
+import "./style/icons.css"
+import "./style/input.css"
+import "./style/search.css"
+import "./style/container.css"
+import "./style/member-card.css"
+import "./style/progress.css"
